e5867936aaffc7e0da4f88ff78f437e30b318340
greeflas <vldmr.kuprienko@gmail.com>
2018-11-05 21:45:02 +0200

    Adds basic authentication

---
+++ src/Controller/Admin/PostController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+final class PostController extends AbstractController
+{
+    public function create()
+    {
+        \var_dump('Create post');
+    }
+}
